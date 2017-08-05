e8637c470ff34731a2457ed68ddeca8d9aff9a10
nbari <nbari@tequila.io>
2017-08-05 22:13:14 +0200

    modified:   crypto_test.go

--- crypto_test.go
+++ crypto_test.go
@@ -3,15 +3,15 @@ package crypto
 import "testing"
 
 func TestGenerateSalt(t *testing.T) {
-	nonces := make(map[string]int)
+	grains := make(map[string]int)
 	for i := 0; i < 1000000; i++ {
-		if nonce, err := GenerateSalt(32); err == nil {
-			nonces[string(nonce)] = i
+		if grain, err := GenerateSalt(32); err == nil {
+			grains[string(grain)] = i
 		} else {
 			t.Error(err)
 		}
 	}
-	if len(nonces) != 1000000 {
+	if len(grains) != 1000000 {
 		t.Error("Salts repeating")
 	}
 }
