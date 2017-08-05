589309c9096fdadc6c42cdc1a52d248d62bcd9c7
nbari <nbari@tequila.io>
2017-08-05 21:05:41 +0200

    new file:   Makefile 	new file:   crypto.go 	new file:   crypto_test.go

---
+++ crypto.go
@@ -0,0 +1,13 @@
+package crypto
+
+import "crypto/rand"
+
+// GenerateSalt returns sequence of random byte values.
+func GenerateSalt(size int) ([]byte, error) {
+	salt := make([]byte, size)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return nil, err
+	}
+	return salt, nil
+}
