84afe4b7f2a84d78a786ca47042e8ef24e8d3796
nbari <nbari@tequila.io>
2017-10-13 11:47:10 +0200

    clean tests

--- scrypt/scrypt_test.go
+++ scrypt/scrypt_test.go
@@ -5,29 +5,32 @@ import (
 )
 
 func TestPassword(t *testing.T) {
-	tests := []struct {
+	tt := []struct {
+		name     string
 		password string
 		len      int
 	}{
-		{"secret", 10},
-		{"secret", 20},
-		{"secret", 30},
-		{"secret", 40},
-		{"secret", 50},
-		{"The quick brown fox jumps over the lazy dog", 32},
-		{"The quick brown fox jumps over the lazy dog", 64},
+		{"len 10", "secret", 10},
+		{"len 20", "secret", 20},
+		{"len 30", "secret", 30},
+		{"len 40", "secret", 40},
+		{"len 50", "secret", 50},
+		{"len 32", "The quick brown fox jumps over the lazy dog", 32},
+		{"len 64", "The quick brown fox jumps over the lazy dog", 64},
 	}
-	for _, tt := range tests {
-		key, err := Create(tt.password, tt.len)
-		if err != nil {
-			t.Fatal(err)
-		}
-		result, err := Verify(tt.password, key)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !result {
-			t.Fatal("Expecting result to be true")
-		}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			key, err := Create(tc.password, tc.len)
+			if err != nil {
+				t.Fatal(err)
+			}
+			result, err := Verify(tc.password, key)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !result {
+				t.Fatal("Expecting result to be true")
+			}
+		})
 	}
 }
